f49d722b916c71c5173777d0f2e1805e490bd70a
NingmengLemon <guanchenxiang@outlook.com>
2024-09-02 00:17:28 +0800

    fix: 改进adb实现

--- src/lemon/spider/android/fetcher.py
+++ src/lemon/spider/android/fetcher.py
@@ -1,40 +1,74 @@
 import subprocess
 import json
 import time
+import random
+
+
+from parse_uixml import parse
+
+
+def run_command(*args):
+    return subprocess.run(
+        args,
+        check=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    ).returncode
 
 
 def dump(tofile="./ui.xml"):
-    subprocess.run(
-        ["adb", "shell", "uiautomator", "dump", "/sdcard/view.xml"],
-        check=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
-    subprocess.run(
-        ["adb", "pull", "/sdcard/view.xml", tofile],
-        check=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
+    run_command("adb", "shell", "uiautomator", "dump", "/sdcard/view.xml")
+    run_command("adb", "pull", "/sdcard/view.xml", tofile)
 
 
-if __name__ == "__main__":
-    from parse_uixml import parse
+def update_no_override(dst: dict, src: dict):
+    dst.update({k: v for k, v in src.items() if k not in dst})
 
-    result = set()
+def main():
+    result: dict[str, str] = {}
     try:
         tmpfile = "./ui.xml"
         cycle = 0
+        no_increase_count = 0
         while True:
             dump(tmpfile)
+            size = len(result)
             with open(tmpfile, "rb") as fp:
-                data = parse(fp.read())
-                print("obtained:", data)
-            result.update(data)
+                data = parse(fp.read(), stored=result)
+            print(*data.items(), sep="\n")
+            update_no_override(result, data)
             cycle += 1
-            print("Cycle", cycle)
+            run_command(
+                "adb",
+                "shell",
+                "input",
+                "swipe",
+                str(random.randint(480, 600)),
+                str(1500 + random.randint(-10, 10)),
+                str(random.randint(480, 600)),
+                str(1000 + random.randint(-10, 10)),
+            )
+            print("Cycle:", cycle, " Add:", len(result) - size, " Total:", len(result))
+
+            if len(result) == size:
+                no_increase_count += 1
+            else:
+                no_increase_count = 0
+            if no_increase_count > 10:
+                break
+            # time.sleep(0.5)
     except KeyboardInterrupt:
         pass
-    print("saving...")
+    print(len(result), "ratings in total, saving...")
     with open(f"./result_{int(time.time())}.json", "w+", encoding="utf-8") as wfp:
-        json.dump(list(result), wfp, ensure_ascii=False, indent=4)
+        json.dump(
+            [{"date": v, "content": k} for k, v in result.items()],
+            wfp,
+            ensure_ascii=False,
+            indent=4,
+        )
+    print("saved!")
+
+
+if __name__ == "__main__":
+    main()
--- src/lemon/spider/android/parse_uixml.py
+++ src/lemon/spider/android/parse_uixml.py
@@ -1,23 +1,54 @@
-from typing import TypedDict
 import re
+from datetime import datetime
+from queue import Queue
 
 from lxml import etree
 
 
-class Rating(TypedDict):
-    date: str
-    content: str
-
-
 _regex_date = re.compile(r"(\d{4}年)?\d{1,2}月\d{1,2}日")
+_regex_detailed_rating = re.compile(r'昵称([^"]+?)评论([^"]+?)')
+_regex_desc_templ = re.compile(r"(^|\s+)[\u4e00-\u9fa5]{4}：")
 
 
-def parse(xmltext: str):
+def rm_desc_templ(rate_content: str):
+    return _regex_desc_templ.sub(" ", rate_content.removeprefix("商品评价:")).strip()
+
+
+def parse(xmltext: str, stored: dict[str, str] | None) -> dict[str, str]:
+    """返回的字典，键为评价内容，值为日期"""
     xml = etree.XML(xmltext, etree.XMLParser())
-    nodes = [e.attrib.get("content-desc", "") for e in xml.findall(".//node")]
+    nodes: list[str] = [
+        e.attrib.get("content-desc", "") for e in xml.findall(".//node")
+    ]
 
-    sus = []
+    if stored is None:
+        stored = {}
+    result = {}
+    date_to_match: Queue[str] = Queue()
+    content_to_match: Queue[str] = Queue()
     for i, node in enumerate(nodes):
         if _regex_date.match(node) and i + 1 < len(nodes):
-            sus.append((node, nodes[i + 1]))
-    return sus
+            date = node
+            if "年" not in date:
+                date = f"{datetime.now().year}年" + date
+
+            content = rm_desc_templ(nodes[i + 1])
+            if content and content not in result and content not in stored:
+                result[content] = date
+                continue
+            date_to_match.put(date)
+            continue
+            # date_to_match.put(date)
+
+        if _regex_detailed_rating.match(node) and i + 4 < len(nodes):
+            content = rm_desc_templ(nodes[i + 4] or nodes[i + 3])
+            if content and content not in result and content not in stored:
+                content_to_match.put(content)
+            continue
+
+    content = ""
+    while not date_to_match.empty() and not content_to_match.empty():
+        content = content_to_match.get()
+        if content not in result:
+            result[content] = date_to_match.get()
+    return result
