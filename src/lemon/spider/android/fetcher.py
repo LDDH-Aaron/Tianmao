e387431b66b75e37529c2e53a81f8420818b0aef
NingmengLemon <guanchenxiang@outlook.com>
2024-09-03 23:38:37 +0800

    feat: add timer for process

--- src/lemon/spider/android/fetcher.py
+++ src/lemon/spider/android/fetcher.py
@@ -39,6 +39,7 @@ def main():
     result: dict[str, str] = {}
     print("Device:", device.info)
     input("就绪。请手动将页面导航至评价列表，然后按下Enter")
+    start_time = time.time()
     try:
         cycle = 0
         no_increase_count = 0
@@ -55,7 +56,7 @@ def main():
                 1800 + random.randint(-10, 10),
                 random.randint(480, 600),
                 1000 + random.randint(-10, 10),
-                duration=0.2
+                duration=0.2,
             )
 
             print("Cycle:", cycle, " Add:", len(result) - size, " Total:", len(result))
@@ -68,7 +69,9 @@ def main():
                 break
     except KeyboardInterrupt:
         pass
-    print(len(result), "ratings in total, saving...")
+    end_time = time.time()
+    print(len(result), "ratings fetched,", f"{end_time-start_time}s cost")
+    print("saving...")
     with open(f"./result_{int(time.time())}.json", "w+", encoding="utf-8") as wfp:
         json.dump(
             [{"date": v, "content": k} for k, v in result.items()],
